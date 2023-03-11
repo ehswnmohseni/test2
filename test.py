c45120a62c9d3b2b9b4d15a8411da0c3d4da9292
ehswn <ehswnmohseni.ir@gmail.com>
2023-03-11 11:54:32 +0330

    twice function deleted!

--- test.py
+++ test.py
@@ -18,16 +18,7 @@ def read_data():
         price = price.replace(",",".")
         n_mileage = float(mileage)
         n_price = float(price)
-        use_data(name,n_price,n_mileage)
 
-def use_data(name,n_price,n_mileage):
-    ready = [n_mileage,n_price]
-    x.append(ready)
-    y.append(name)
-    cls = tree.DecisionTreeClassifier()
-    cls = clf.fit(x, y)
-    answer = clf.predict([in_price,in_mileage])
-    print(answer)
 
 read_data()
 
